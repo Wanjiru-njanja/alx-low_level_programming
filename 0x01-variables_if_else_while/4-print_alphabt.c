eaa0376f2f674d48129c8a151aff53094c61a759
Wanjiru-njanja <wanjirunjanja@gmail.com>
2022-09-12 14:19:44 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints letters in lowercase
+ *
+ * Description: using main function
+ * this program prints "letters in lowercase"
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+}
+if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
