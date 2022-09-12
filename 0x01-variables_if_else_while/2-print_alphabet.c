884f6def3de436a4ce960f8825104fd04a0a4bdd
Wanjiru-njanja <wanjirunjanja@gmail.com>
2022-09-12 13:51:28 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print alphabets are in lowercase
+*
+* Description: using the main function
+* this program prints "letters in lowercase"
+* Return: 0
+*/
+int main(void)
+{
+char c;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch)
+}
+putchar('\n');
+return (0);
+}
