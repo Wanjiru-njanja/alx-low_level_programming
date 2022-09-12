cd1ab02abccb974a93d879e0db460a8100a0ae8a
Wanjiru-njanja <wanjirunjanja@gmail.com>
2022-09-12 15:47:57 -0700

    Task 6

---
+++ 0x00-hello_world/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: using the main function
+ * Return: 0
+ */
+int main (void)
+
+{
+int i;
+
+for (i =48; i <= 57; i++)
+{
+putchar(i);
+}
+
+putchar('\n');
+
+return (0);
+}
