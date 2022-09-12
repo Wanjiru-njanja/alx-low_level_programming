08647af1d0e47c1249df9fe0619158c13af2e3a5
Wanjiru-njanja <wanjirunjanja@gmail.com>
2022-09-12 11:57:11 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print the string in printf function
+*
+* Description: using the main function
+* this program prints "with proper grammar, but the outcome is a piece of art"
+* followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0
+}
