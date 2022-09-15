afbcfda3e22a94cf99ab5d14d71ba43d778c7452
Wanjiru Njanja <wanjirunjanja@gmail.com>
2022-09-15 15:53:36 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _islower - checks for lower case characters
- *
+ * @c: is the int that will use for argumennt of the function
  * Return: 0
  */
 int _islower(int c)
 {
-	if ( c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
