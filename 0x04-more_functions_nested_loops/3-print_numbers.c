654683b464787abce66ce2900e4715fc422a8e68
Wanjiru Njanja <wanjirunjanja@gmail.com>
2022-09-16 19:13:49 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_number - func that prints all the numbers from
+* 0 to 9
+*
+* Return: return nothing
+*/
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(0);
+	}
+	putchar(0);
+}
