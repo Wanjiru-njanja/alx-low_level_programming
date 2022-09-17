4b8ef0bb6c50c508e31b11340c6bd8ca1aad19bf
Wanjiru Njanja <wanjirunjanja@gmail.com>
2022-09-16 20:12:06 -0700

    Task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,15 +3,14 @@
 /**
  * _isdigit - checks whether a character is a digit or not
  * @c: tested character
- * Return: 1 if it is, 0 otherwisw
+ * Return: 1 if it is, 0 otherwise
  */
 
 int _isdigit(int c)
 {
-	if ((c >= 48 && (c <= 57))
+	if ((c >= 48) && (c <= 57))
 	{
 		return (1);
 	}
-
 	return (0);
 }
