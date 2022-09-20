b96148ee2b8163a8c338a183276bb8d8e31bc475
Wanjiru Njanja <wanjirunjanja@gmail.com>
2022-09-20 15:05:17 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
-/** _puts - function that prints a string
- * @str: string to print
- *
- * Description: prints a string
- * On success: returns no error
- */
+/**
+* _puts - function that prints a string
+* @str: string to print
+*
+* Description: prints a string
+* On success: returns no error
+*/
 
 void _puts(char *str)
 {
