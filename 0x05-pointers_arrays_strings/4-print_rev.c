3731f3ee39cd8fb797954e6dd4771e1bd7b4c42c
Wanjiru Njanja <wanjirunjanja@gmail.com>
2022-09-20 14:02:41 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* print_rev - function that prints a string in reverse order
+* @s: String to reverse
+* Return: nothing
+*/
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
+}
