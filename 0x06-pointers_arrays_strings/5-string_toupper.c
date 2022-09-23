7d3c325425bb6b8dea4abdaa866d475943e412ca
Wanjiru Njanja <wanjirunjanja@gmail.com>
2022-09-22 20:10:20 -0700

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,10 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
+			str[i] = str[i] - 32;
 		i++;
 	}
 	return (str);
