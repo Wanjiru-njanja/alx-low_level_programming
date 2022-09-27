2025162bb65a6c396ef14d0abe77d6843d76a336
Wanjiru Njanja <wanjirunjanja@gmail.com>
2022-09-27 02:00:34 -0700

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _memcpy - function that copies memory area
+* @src: memory area to be copied from
+* @dest: memory area to be copied to
+* @n: number of bytes to be copies
+*
+* Return: pointer to the copied memory block
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
