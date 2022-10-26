f11d3ada7878cbbeeafd2a556de4af8dea32bd24
Wanjiru Njanja <wanjirunjanja@gmail.com>
2022-10-26 06:16:47 -0700

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+void start_up_func(void)__attribute__((constructor));
+
 /**
- * printf - function that prints characters
+ * start_up_func - function that get's executed before the main function
+ *
  * Return: void
  */
-
-int main(void)
+void start_up_func(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
-	       " bore my house upon my back!\n" );
+	       "I bore my house upon my back!\n");
 }
