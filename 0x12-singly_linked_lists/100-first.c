d9e566dd7e6d15901a8d8ca7634162b0eacd0a0c
Wanjiru Njanja <wanjirunjanja@gmail.com>
2022-10-26 06:12:22 -0700

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * printf - function that prints characters
+ * Return: void
+ */
+
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       " bore my house upon my back!\n" );
+}
