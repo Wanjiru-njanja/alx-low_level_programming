91cb13806a894eef4c176e40e9a4bc816d5bc698
Wanjiru Njanja <wanjirunjanja@gmail.com>
2022-11-01 20:32:08 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,6 +20,7 @@ int check_valid_string(const char *b)
 	return (1);
 }
 
+
 /**
  * binary_to_uint - converts a binary to an unsigned int
  * @b: the binary number as a string
@@ -32,8 +33,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!check_valid_string(b))
 		return (0);
-	while (b[str_len]!= '\0')
-
+	while (b[str_len] != '\0')
 		str_len++;
 	while (str_len)
 	{
